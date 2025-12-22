1806203b59a5ad701b63b11106762fe46f078154
Mio <mio@mio19.uk>
2025-12-22 17:02:32 +1100

    #

--- flake.nix
+++ flake.nix
@@ -178,7 +178,7 @@
                   obsidian
                   gamescope
                   gnome-calendar
-                  chromium
+                  #chromium
                   aseprite
                   wiliwili
                   #krdp
